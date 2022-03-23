aa1db5161cdcc1678fc4f28fd370044095769277
TorobekKalmanbetov <100470974+TorobekKalmanbetov@users.noreply.github.com>
2022-03-23 21:36:43 +0500

    Add files via upload

---
+++ mans-shop/js/index.js
@@ -0,0 +1,15 @@
+/*window.onscroll = function() {
+    checkMarginToTop();
+};
+
+var nav = document.getElementById("header_menu");
+
+var sticky = nav.offsetTop;
+
+function checkMarginToTop() {
+        if (window.pageYOffset > sticky) {
+        nav.classList.add("sticky");
+    } else {
+        nav.classList.remove("sticky");
+    }
+}*/
